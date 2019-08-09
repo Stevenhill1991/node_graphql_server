db355ac6f0eb7207e6ba9592eaf6f596fadb2738
Steven Hill <stevenhill1991@hotmail.com>
2019-08-10 00:24:19 +0100

    fix dockerfile to work when building from scratch

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
-require('dotenv').config();
+
 
 const typeDef = `
   type Query
